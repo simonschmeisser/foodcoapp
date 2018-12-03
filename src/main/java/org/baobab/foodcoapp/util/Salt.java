9ad20fa9275724a154061a05908b0df964dc6398
Simon Schmeisser <s.schmeisser@gmx.net>
2018-12-03 16:55:35 +0100

    Create Salt.java
    Add a dummy salt file so this can compile. To create actual installation apks you need the real salt or PINs won't match

---
+++ src/main/java/org/baobab/foodcoapp/util/Salt.java
@@ -0,0 +1,8 @@
+package org.baobab.foodcoapp.util;
+
+public class Salt {
+    //this is installation specific to "protect" the "highly sensitive" pins, ask @simonschmeisser
+    //for the one used by Kornkammer
+    static byte[] SALT = "foo bar".getBytes();
+
+}
